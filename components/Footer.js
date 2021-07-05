86f72babe8b533142aa7a052cd3e8a2aee0d7148
aminects <amineamine19961996@gmail.com>
2021-07-05 20:57:18 +0300

    FIX: footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,45 +1,33 @@
-import Link from 'next/link';
 import styles from "../styles/Footer.module.css";
 import SVGInstagram from "./icons/SVGInstagram";
 import SVGFacebook from "./icons/SVGFacebook";
 import SVGLinkedin from "./icons/SVGLinkedin";
 import SVGGithub from "./icons/SVGGithub";
-import { INSTAGRAM,FACEBOOK,LINKEDIN,GITHUB } from '../helpers/global-variables';
+import {
+  INSTAGRAM,
+  FACEBOOK,
+  LINKEDIN,
+  GITHUB,
+} from "../helpers/global-variables";
+
+const ICON = (SVG, url) => (
+  <span className={styles.icons} onClick={() => window.open(url)}>
+    {SVG}
+  </span>
+);
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <div>
-                <span className={styles.icons}>
-                    <Link
-                        href={GITHUB}
-                    >
-                        <a target="_blank" rel="noopener noreferrer"><SVGGithub /></a>
-                    </Link>
-                </span>
-                <span className={styles.icons}>
-                    <Link
-                        href={LINKEDIN}
-                    >
-                        <a target="_blank" rel="noopener noreferrer"><SVGLinkedin /></a>
-                    </Link>
-                </span>
-                <span className={styles.icons}>
-                    <Link
-                        href={INSTAGRAM}
-                    >
-                        <a target="_blank" rel="noopener noreferrer"><SVGInstagram /></a>
-                    </Link>
-                </span>
-                <span className={styles.icons}>
-                    <Link
-                        href={FACEBOOK}
-                    >
-                        <a target="_blank" rel="noopener noreferrer"><SVGFacebook /></a>
-                    </Link>
-                </span>
-            </div>
-            <div className="mt-4">Powered by <b>Amine-Next-JS</b></div>
-        </footer >
-    )
+  return (
+    <footer className={styles.footer}>
+      <div>
+        {ICON(<SVGGithub />, GITHUB)}
+        {ICON(<SVGLinkedin />, LINKEDIN)}
+        {ICON(<SVGInstagram />, INSTAGRAM)}
+        {ICON(<SVGFacebook />, FACEBOOK)}
+      </div>
+      <div className="mt-4">
+        Powered by <b>Amine-Next-JS</b>
+      </div>
+    </footer>
+  );
 }
