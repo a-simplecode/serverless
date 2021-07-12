7844718fa8e31aa0211791abe1a1583672a1eb47
aminects <amineamine19961996@gmail.com>
2021-07-12 23:58:43 +0300

    FIX: optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,34 @@
+import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../styles/globals.css'
-import { ToastContainer, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "../styles/globals.css";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
-  return <>
-  <Component {...pageProps} />
-  <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick={true}
-      rtl={false}
-      pauseOnHover
-      transition={Slide}
-    />
-  </>
+  return (
+    <>
+      <Head>
+        <title>Amine Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0 width=device-width" />
+        <meta
+          name="description"
+          content="Connect with a great web developer for building websites"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnHover
+        transition={Slide}
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
